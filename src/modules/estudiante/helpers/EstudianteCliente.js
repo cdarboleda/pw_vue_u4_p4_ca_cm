9addb92397bb0ffda99bb0e52314ee932939bc63
Cristian Arboleda <cdarboleda@uce.edu.ec>
2023-08-22 19:41:01 -0500

    Taller Cliente Vue asegurada

--- src/modules/estudiante/helpers/EstudianteCliente.js
+++ src/modules/estudiante/helpers/EstudianteCliente.js
@@ -27,8 +27,10 @@ const obtenerEstudianteAPI  = async (cedula) =>{
 
 const obtenerEstudianteAPIAxios  = async (cedula) =>{
     console.log('AXIOS2')
+    const headers = {Authorization :"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5Mjc0OTMzOSwiZXhwIjoxNjkyNzU5MzM5fQ.S4FKSxtl2fhCDOKtExK8LoVoJiFhLNJG_kL7F2klSeFuGHHySIod7Wy_4AxVXEEXXweyYORJoQdkOgrPxZw3TA",
+    Mensaje : "valor"}
     const data = axios
-        .get(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${cedula}`)
+        .get(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${cedula}`, {headers : headers})
         .then(r=>r.data);
 
     console.log(data);
@@ -36,22 +38,28 @@ const obtenerEstudianteAPIAxios  = async (cedula) =>{
 }
 
 const ingresarEstudiante= (bodyEstudiante) => {
+    const headers = {Authorization :"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5Mjc0OTMzOSwiZXhwIjoxNjkyNzU5MzM5fQ.S4FKSxtl2fhCDOKtExK8LoVoJiFhLNJG_kL7F2klSeFuGHHySIod7Wy_4AxVXEEXXweyYORJoQdkOgrPxZw3TA",
+    Mensaje : "valor"}
     axios
         .post(`http://localhost:8080/API/v1.0/Matricula/estudiantes`
-        , bodyEstudiante)
+        , bodyEstudiante, {headers : headers})
         .then(r=>r.data);
 }
 
 const actualizarEstudiante = (bodyEstudiante, id) => {
+    const headers = {Authorization :"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5Mjc0OTMzOSwiZXhwIjoxNjkyNzU5MzM5fQ.S4FKSxtl2fhCDOKtExK8LoVoJiFhLNJG_kL7F2klSeFuGHHySIod7Wy_4AxVXEEXXweyYORJoQdkOgrPxZw3TA",
+    Mensaje : "valor"}
     axios
     .put(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${id}`
-    , bodyEstudiante)
+    , bodyEstudiante,{headers : headers})
     .then(r=>r.data);
 }
 
 const eliminarEstudiante = async (id) => {
+    const headers = {Authorization :"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY5Mjc0OTMzOSwiZXhwIjoxNjkyNzU5MzM5fQ.S4FKSxtl2fhCDOKtExK8LoVoJiFhLNJG_kL7F2klSeFuGHHySIod7Wy_4AxVXEEXXweyYORJoQdkOgrPxZw3TA",
+    Mensaje : "valor"}
     axios
-        .delete(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${id}`)
+        .delete(`http://localhost:8080/API/v1.0/Matricula/estudiantes/${id}`,{headers : headers})
         .then(r=>r.data)
 }
 
